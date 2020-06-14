95b4d1b9de056335d74a18c795cd4a793801d0a9
LucianoWebDev <LucianoWebDev@users.noreply.github.com>
2020-06-14 10:48:13 +0200

    first commit

---
+++ master.js
@@ -0,0 +1,20 @@
+let output = document.getElementById('result')
+let x = document.getElementById('words')
+let xx, xxx
+let y = document.getElementById('btn')
+let btn = document.getElementById('btn')
+
+x.addEventListener('input', function () {
+    let arr = x.value.split(' ')
+    xx = arr.map((n) =>
+        n.length === 5 || /pizza/i.test(n)
+            ? '🍕'
+            : /hawai/gi.test(n)
+            ? (alert('Are you Crazy??'),
+              (x.value = ''),
+              (output.innerHTML = ''))
+            : n
+    )
+    document.getElementById('result').innerHTML = xx.join(' ')
+    console.log(arr)
+})
